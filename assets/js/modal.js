a628fd39d00eb81d68e8d49316d589522c76be01
mp-di <mp-di@MP-DIGITAL>
2023-02-01 17:10:46 +0800

    .

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,17 +1,30 @@
-const modal = document.querySelector(".modal");
+const terms = document.getElementById("terms");
+const policy = document.getElementById("policy");
 const trigger = document.querySelector(".trigger");
-const closeButton = document.querySelector(".close-button");
+const trigger1 = document.querySelector(".trigger1");
+const closeButton = document.getElementById("tClose");
+const closeButton1 = document.getElementById("pClose");
 
-function toggleModal() {
-    modal.classList.toggle("show-modal");
+function toggleModal(modal) {
+	console.log(modal);
+	if (modal === "terms") {
+		terms.classList.toggle("show-modal");
+	}
+	if (modal === "policy") {
+		policy.classList.toggle("show-modal");
+	}
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
+	console.log(event.target);
+	if (event.target === trigger) toggleModal("terms");
+	if (event.target === closeButton) toggleModal("terms");
+	if (event.target === trigger1) toggleModal("policy");
+	if (event.target === closeButton1) toggleModal("policy");
 }
 
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
+trigger.addEventListener("click", toggleModal("terms"));
+trigger1.addEventListener("click", toggleModal("policy"));
+closeButton.addEventListener("click", toggleModal("terms"));
+closeButton1.addEventListener("click", toggleModal("policy"));
 window.addEventListener("click", windowOnClick);
